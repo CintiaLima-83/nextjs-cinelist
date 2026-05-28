84bac7f939418b45fadf1f703446d45c81a3c682
Cintia Lima <limacintia82@gmail.com>
2026-05-28 19:03:10 -0300

    12345

--- app/lib/api/axios.ts
+++ app/lib/api/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const tmdbApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_TMDB_API_URL,
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_TOKEN}`,
+    Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
     "Content-Type": "application/json",
   },
 });
