a8a975769875934925e88f6f09f277987c0baa4f
Cintia Lima <limacintia82@gmail.com>
2026-05-28 19:09:27 -0300

    1234567

--- app/lib/api/axios.ts
+++ app/lib/api/axios.ts
@@ -10,5 +10,6 @@ const tmdbApi = axios.create({
 
 export default tmdbApi;
 
-console.log("API URL:", JSON.stringify(process.env.NEXT_PUBLIC_TMDB_API_URL));
-console.log("Token:", JSON.stringify(process.env.NEXT_PUBLIC_TMDB_API_TOKEN));
+console.log("API URL:", JSON.stringify(process.env.TMDB_API_URL));
+console.log("Token:", JSON.stringify(process.env.TMDB_API_TOKEN));
+console.log("Image URL:", JSON.stringify(process.env.TMDB_API_IMG_URL));
