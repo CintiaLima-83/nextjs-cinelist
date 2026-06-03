e6796cbc6167f7097fcb5b7521150fe36de84c0a
Cintia Lima <limacintia82@gmail.com>
2026-06-03 09:36:00 -0300

    refactor: revisão completa do projeto com melhorias visuais, ajustes de responsividade e refinamento das páginas inicial e de detalhes

---
+++ __mocks__/swiper/react.js
@@ -0,0 +1,4 @@
+import React from "react";
+
+export const Swiper = ({ children }) => <div className="mock-swiper">{children}</div>;
+export const SwiperSlide = ({ children }) => <div className="mock-swiper-slide">{children}</div>;
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom';
+
+jest.mock("swiper/react", () => ({
+  Swiper: ({ children }) => <div className="mock-swiper">{children}</div>,
+  SwiperSlide: ({ children }) => <div className="mock-swiper-slide">{children}</div>,
+}));
+
+jest.mock("swiper/modules", () => ({
+  Autoplay: {},
+}));
